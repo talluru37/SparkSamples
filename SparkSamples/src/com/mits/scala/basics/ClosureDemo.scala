e386824ea25fbaafb8cb0a7832f782efae8f0e72
talluru37 <talluru37@gmail.com>
2018-09-01 17:45:27 +0800

    Scala samples

---
+++ SparkSamples/src/com/mits/scala/basics/ClosureDemo.scala
@@ -0,0 +1,14 @@
+package com.mits.scala.basics
+
+object ClosureDemo {
+  
+ 
+   def main(args: Array[String]) {
+      println( "multiplier(1) value = " +  multiplier(1) )
+      println( "multiplier(2) value = " +  multiplier(2) )
+   }
+   var factor = 3
+   val multiplier = (i:Int) => i * factor
+
+  
+}
